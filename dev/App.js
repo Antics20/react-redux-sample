906014edfc2cf11fc65ffef84788a6a1e68b2296
Antics20 <baciu.adrian86@yahoo.com>
2017-08-03 14:27:54 +0300

    Visual components

--- dev/App.js
+++ dev/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from './components/Header';
+import AddArticle from './components/AddArticle';
+import UpdateArticle from './components/UpdateArticle';
+import Visualcomp from './Components/Visualcomp';
+import './App.css';
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <p>Hello</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <div className="row">
+          <div className="col-md-4">
+            <AddArticle />
+            <UpdateArticle />
+
+          </div>
+          <div className="col-md-8">
+  <Visualcomp />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
---
+++ dev/Components/AddArticle.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class AddArticle extends Component {
+  render() {
+    return(
+      <form>
+        <h3>Add Article</h3>
+        <div className="form-group">
+          <label for="textTitle">Title</label>
+          <input type="text" className="form-control" id="textTitle" />
+          <label for="textDescription">Description</label>
+          <textarea className="form-control" rows="10" id="textDescription"></textarea>
+        </div>
+        <button type="submit" className="btn btn-success">Save</button>
+      </form>
+    );
+  }
+}
+
+export default AddArticle;
